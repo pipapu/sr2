626c43461e27dc13855adf18f5980cc8baedcd5e
Axel G. Rossberg <axel@rossberg.net>
2021-07-03 15:24:08 +0200

    Stable code generates SR relations for NS and hypothetical community

--- MultipleResources.R
+++ MultipleResources.R
@@ -1,5 +1,37 @@
 # Implementation and use example of a large number of resource size spectra in mizer
 library(mizer)
+source("getRecruitment.R")
+
+## but fix:
+setInitialValues <- function (params, sim) 
+{
+  no_t <- dim(sim@n)[1]
+  if (!identical(dim(sim@n)[2:3], dim(params@initial_n))) {
+    stop("The consumer size spectrum of the simulation in `sim` has a ", 
+         "different size from that in `params`.")
+  }
+  if (!identical(length(sim@n_pp[no_t, ]), length(params@initial_n_pp))) {
+    stop("The resource size spectrum of the simulation in `sim` has a ", 
+         "different size from that in `params`.")
+  }
+  if (!identical(length(sim@n_other[no_t, ]), length(params@initial_n_other))) {
+    stop("The number of other components in the simulation in `sim` is ", 
+         "different from that in `params`.")
+  }
+  if (!identical(length(sim@effort[no_t, ]), length(params@initial_effort))) {
+    stop("The number of gears in the simulation in `sim` is ", 
+         "different from that in `params`.")
+  }
+  if (!identical(dimnames(sim@effort)[[2]], names(params@initial_effort))) {
+    stop("The gears in the simulation in `sim` have different names ", 
+         "from those in `params`.")
+  }
+  params@initial_n[] <- sim@n[no_t, , ]
+  params@initial_n_pp[] <- sim@n_pp[no_t, ]
+  params@initial_n_other[] <- sim@n_other[no_t, ]
+  params@initial_effort[] <- sim@effort[no_t, ]  ## fixed here
+  params
+}
 
 ###### Implementation: ######
 
@@ -141,10 +173,10 @@ plot(sort(diet_props/(1-diet_props),decreasing = T),seq_along(diet_props)/nrow(d
  
 ### Now an example with a large number or resource spectra and fish species
 
-nS <- 20  # you can make this arbitrarily large
+nS <- 100  # you can make this arbitrarily large
 nR <- 2*nS
 nu <- 0.6  # target diet partitioning exponent
-interaction_spread_sigma <- sqrt(2*log(nR+nS))/nu
+interaction_spread_sigma <- sqrt(2*log(0.85*nS))/nu
 
 initial_inter <- 
   matrix(exp(rnorm(nS*nS,-interaction_spread_sigma^2/2,interaction_spread_sigma)),
@@ -173,7 +205,7 @@ with(species_params(params_npps),plot(gamma/w_inf^(0.1)~w_inf,log="xy"))
 
 # Fix gamma as promised above:
 species_params(params_npps)$gamma <-
-  species_params(params_npps)$gamma * inter_row_max * 100
+  species_params(params_npps)$gamma * inter_row_max * 3 #10
 with(species_params(params_npps),plot(gamma/w_inf^(0.1)~w_inf,log="xy"))
 
 # The species size spectrum
@@ -181,7 +213,10 @@ plot(log(rev(seq(nrow(species_params(params_npps)))))~log(sort(species_params(pa
 lm(log(sort(species_params(params_npps)$w_inf))~log(rev(seq(nrow(species_params(params_npps))))))
 
 # First simulation
-out3 <- project(params_npps,t_max = 1000)
+out3 <- project(params_npps,t_max = 100)
+#params_npps_hold <- params_npps
+params_npps <- setInitialValues(params_npps, out3)
+plotBiomass(out3)
 
 # Remove species that went extinct
 species_to_remove <- tail(getBiomass(out3),n = 1) < 1e9
@@ -192,6 +227,7 @@ params_npps2@other_params[["n_pps"]]$interaction <-
 
 # Second simulation
 out4 <- project(params_npps2,t_max = 1000)
+params_npps2 <- setInitialValues(params_npps2, out4)
 plotBiomass(out4)
 
 # Remove species that went extinct
@@ -203,6 +239,7 @@ params_npps3@other_params[["n_pps"]]$interaction <-
 
 # Third simulation
 out5 <- project(params_npps3,t_max = 1000)
+params_npps3 <- setInitialValues(params_npps3, out5)
 plotBiomass(out5)
 
 # Remove species that went extinct
@@ -214,6 +251,7 @@ params_npps4@other_params[["n_pps"]]$interaction <-
 
 # Fourth simulation (there should be no more extinctions)
 out6 <- project(params_npps4,t_max = 1000)
+params_npps4 <- setInitialValues(params_npps4, out6)
 plotBiomass(out6)
 
 # Compare initial and final species size spectrum:
@@ -239,6 +277,75 @@ barplot(BB_percentage[,seq(1,ncol(BB_percentage),length.out = 30)],col = coul, b
 # Compare this e.g. with Fig. 10 here: http://axel.rossberg.net/paper/Shephard2012ICES-JMS_Fishing_drives_LSI.pdf
 
 ## Animation of spectra takes a while, but result is interesting!  Note that, contrary to what one might expect, variability at small sizes is larger than at intermediate sizes. 
-animateSpectra(out6,total = T)
+#animateSpectra(out6,total = T)
 
 # To compute diet partitioning functions (http://axel.rossberg.net/paper/Rossberg2011PRSB_Diet_Partitioning.pdf), we need to make getDiet work by registering params@other_encounter for all background components.  This requires some function closer trickery....
+
+
+#params_npps4@initial_effort <- params@initial_effort   # !!! Hack to make it work
+params_npps4@gear_params$knife_edge_size[] <- 0
+for(target_sp in 1:S){
+  paramsF <- params_npps4 #make a copy for iterative change.
+  ff <- round(c(0,10^seq(log10(0.01),log10(20),length.out = 60)),3)
+  rec <- rep(0,length(ff))
+  ssb <- rep(0,length(ff))
+  no_sp <- nrow(paramsF@species_params)
+  species_params(paramsF)$species[target_sp]
+  my_gear <- as.character(unique(paramsF@gear_params$gear))[1]
+  for(i in seq_along(ff)){
+    fishing <- ff[i]
+    names(fishing) <- c(my_gear)
+    cb <- matrix(as.numeric(seq_len(no_sp)==target_sp),nrow=1,ncol=no_sp,
+                 dimnames = list(gear = c(my_gear), sp=row.names(species_params(paramsF))))
+    paramsF <-
+      setFishing(params = paramsF,
+                 catchability = cb)
+    outF <- project(paramsF, effort = fishing, t_max = 20)
+    ssb[i] <- tail(getBiomass(outF)[,target_sp],1)  # getSSB does not work
+    paramsF <- setInitialValues(paramsF,outF)
+    rec[i] <- getRecruitment(paramsF)[target_sp]
+    if(i > 1 && rec[i] < 0.001*max(rec)) break;
+  }
+  plot(rec ~ ssb,xlim = c(0,max(ssb)), ylim=c(0,max(rec)), xlab="biomass")
+  feasible_fs <- which(rec > 0.01*max(rec))
+  tag_fs <- 
+    unique(mapply(function(f) which.min(abs(ff-f)),seq(0,max(ff[feasible_fs]),length.out = 7)))
+  points(x = ssb[tag_fs], y=rec[tag_fs], col = "blue", pch = 4)
+  text(x = ssb[tag_fs], y=rec[tag_fs],labels = paste0("F = ",ff[tag_fs]),pos = ifelse(ssb[tag_fs]>0.6*max(ssb),2,4), col = "blue")
+  title(species_params(paramsF)$species[target_sp])
+}
+
+
+params <- newMultispeciesParams(NS_species_params,inter)
+paramsEQ <- setInitialValues(params,project(params))
+paramsEQ@gear_params$knife_edge_size[] <- 0
+for(target_sp in 1:12){
+  paramsF <- paramsEQ #make a copy for iterative change.
+  ff <- round(c(0,10^seq(log10(0.01),log10(20),length.out = 60)),3)
+  rec <- rep(0,length(ff))
+  ssb <- rep(0,length(ff))
+  no_sp <- nrow(paramsF@species_params)
+  species_params(paramsF)$species[target_sp]
+  my_gear <- as.character(unique(paramsF@gear_params$gear))[1]
+  for(i in seq_along(ff)){
+    fishing <- ff[i]
+    names(fishing) <- c(my_gear)
+    cb <- matrix(as.numeric(seq_len(no_sp)==target_sp),nrow=1,ncol=no_sp,
+                 dimnames = list(gear = c(my_gear), sp=row.names(species_params(paramsF))))
+    paramsF <-
+      setFishing(params = paramsF,
+                 catchability = cb)
+    outF <- project(paramsF, effort = fishing, t_max = 10)
+    ssb[i] <- tail(getBiomass(outF)[,target_sp],1)  # getSSB does not work
+    paramsF <- setInitialValues(paramsF,outF)
+    rec[i] <- getRecruitment(paramsF)[target_sp]
+    if(i > 1 && rec[i] < 0.001*max(rec)) break;
+  }
+  plot(rec ~ ssb,xlim = c(0,max(ssb)), ylim=c(0,max(rec)), xlab="biomass")
+  feasible_fs <- which(rec > 0.01*max(rec))
+  tag_fs <- 
+    unique(mapply(function(f) which.min(abs(ff-f)),seq(0,max(ff[feasible_fs]),length.out = 7)))
+  points(x = ssb[tag_fs], y=rec[tag_fs], col = "blue", pch = 4)
+  text(x = ssb[tag_fs], y=rec[tag_fs],labels = paste0("F = ",ff[tag_fs]),pos = ifelse(ssb[tag_fs]>0.6*max(ssb),2,4), col = "blue")
+  title(species_params(paramsF)$species[target_sp])
+}
